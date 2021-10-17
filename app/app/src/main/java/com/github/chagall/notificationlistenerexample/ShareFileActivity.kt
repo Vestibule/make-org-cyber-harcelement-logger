2ef4c99feea688ee2e225b86be2e8a21755b37b9
Nicolas Li <nicolas@ovrsea.com>
2021-10-17 12:40:32 +0200

    fix(screenshot): fix body type

--- app/app/src/main/java/com/github/chagall/notificationlistenerexample/ShareFileActivity.kt
+++ app/app/src/main/java/com/github/chagall/notificationlistenerexample/ShareFileActivity.kt
@@ -6,10 +6,7 @@ import android.os.Bundle
 import android.os.Parcelable
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import okhttp3.MediaType
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
+import okhttp3.*
 import okhttp3.RequestBody.Companion.asRequestBody
 import okio.BufferedSink
 import okio.Okio
@@ -54,14 +51,15 @@ class ShareFileActivity : AppCompatActivity() {
         if (imageUri != null) {
             // Update UI to reflect image being shared
             Log.i("NCC - path", imageUri.path.toString())
-            val inputStream = contentResolver.openInputStream(imageUri)
+            val inputStream = contentResolver.openInputStream(imageUri)!!
             Log.i("NCC - size", inputStream?.available().toString())
 
-            val body = inputStream?.asRequestBody()
+            val formBody = MultipartBody.Builder().setType(MultipartBody.FORM).addFormDataPart("screenshot", null, body=inputStream.asRequestBody()).build()
+
             val request = Request.Builder()
                 .url("http://ec2-35-180-193-101.eu-west-3.compute.amazonaws.com:3000/message/screenshot")
                 .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZvb0BiYXIuYmF6Iiwic3ViIjo2LCJpYXQiOjE2MzQ0NTg4MjYsImV4cCI6MTY2NTk5NDgyNn0.8ZEnhCy25dkYNCG-F0Ag0HRXjEfTaRZle8vBcY_5afk")
-                .post(body!!)
+                .post(formBody)
                 .build()
 
             try {
